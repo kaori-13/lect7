0ce34fc8928ab899e79342d8e0c9cbe4b6d7b771
Fanny <galaxysgra@gmail.com>
2025-11-07 09:19:11 +0800

    Initial Commit

---
+++ lect7/lect7App.swift
@@ -0,0 +1,17 @@
+//
+//  lect7App.swift
+//  lect7
+//
+//  Created by Fanny on 2025/11/7.
+//
+
+import SwiftUI
+
+@main
+struct lect7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
